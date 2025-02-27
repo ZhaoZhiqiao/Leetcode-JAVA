728c5eb15dca0130d1e71e3fe29bb5418f2e8ee7
黑羽彻 <zhaozhiqiao.dev@gmail.com>
2025-02-27 20:51:04 +0800

    [update]完成题目: 基本计算器

--- src/TopInterview150_middle50.java
+++ src/TopInterview150_middle50.java
@@ -14,7 +14,7 @@ public class TopInterview150_middle50 {
         int[][] matrix = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
         String str1 = "/.../a/../b/c/../d/./";
         String str2 = "(]";
-        String[] strings = {"4","13","5","/","+"};
+        String[] strings = {"4", "13", "5", "/", "+"};
         char[][] board = {{'5', '3', '.', '.', '7', '.', '.', '.', '.'}
                 , {'6', '.', '.', '1', '9', '5', '.', '.', '.'}
                 , {'.', '9', '8', '.', '.', '.', '.', '6', '.'}
@@ -144,5 +144,36 @@ public class TopInterview150_middle50 {
         return stack.pop();
     }
 
+    public int calculate(String s) {
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(1);
+
+        int sign = 1, result = 0, n = s.length(), i = 0;
+        while (i < n) {
+            if (s.charAt(i) == ' ') {
+                i++;
+            } else if (s.charAt(i) == '+') {
+                sign = !stack.isEmpty() ? stack.peek() : 1;
+                i++;
+            } else if (s.charAt(i) == '-') {
+                sign = !stack.isEmpty() ? -stack.peek() : 1;
+                i++;
+            } else if (s.charAt(i) == '(') {
+                stack.push(sign);
+                i++;
+            } else if (s.charAt(i) == ')') {
+                stack.pop();
+                i++;
+            } else {
+                int num = 0;
+                while (i < n && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + s.charAt(i) - '0';
+                    i++;
+                }
+                result += (sign * num);
+            }
+        }
+        return result;
+    }
 }
 
